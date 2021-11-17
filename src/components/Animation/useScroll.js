da62ea7e2b56cc4dde59bc85381495d683ac6983
unknown <minthantkyaw47@gmail.com>
2021-11-17 17:21:33 +0630

    fixed animation threshold

--- src/components/Animation/useScroll.js
+++ src/components/Animation/useScroll.js
@@ -1,7 +1,7 @@
 import { useInView } from "react-intersection-observer";
 import { useAnimation } from "framer-motion/dist/es/index";
 
-export default function useScroll(thres = 0.2) {
+export default function useScroll(thres = 0.05) {
   const controls = useAnimation();
   const [element, view] = useInView({ threshold: thres });
 
